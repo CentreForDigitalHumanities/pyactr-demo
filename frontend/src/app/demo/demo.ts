cf3f37474baf508e522449a06cc81163a5dd9b9d
Luka van der Plas <lukavdplas@mailfence.com>
2025-11-24 17:28:40 +0100

    add pyodide

--- frontend/src/app/demo/demo.ts
+++ frontend/src/app/demo/demo.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadPyodide, version as pyodideVersion } from 'pyodide';
 
 @Component({
     selector: 'app-demo',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
     templateUrl: './demo.html',
     styleUrl: './demo.scss',
 })
-export class Demo {
-
+export class Demo implements OnInit {
+    ngOnInit() {
+        loadPyodide({
+            indexURL: `https://cdn.jsdelivr.net/pyodide/v${pyodideVersion}/full/`,
+        }).then(py => {
+            py.runPython('print("test")')
+        });
+    }
 }
