b30d77fde04b6e38c715be0c3f8993c19b6ee927
Luka van der Plas <lukavdplas@mailfence.com>
2025-11-24 17:22:29 +0100

    add demo component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Demo } from './demo/demo';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: Home,
+    },
+    {
+        path: 'demo',
+        component: Demo,
     },
     {
         path: '',
