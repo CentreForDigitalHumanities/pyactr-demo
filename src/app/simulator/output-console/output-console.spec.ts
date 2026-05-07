9fb0d9e334a2332f79b30f2e46938201f0579772
Luka van der Plas <lukavdplas@mailfence.com>
2026-05-07 12:45:43 +0200

    set input in output-console test

--- src/app/simulator/output-console/output-console.spec.ts
+++ src/app/simulator/output-console/output-console.spec.ts
@@ -1,24 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OutputConsole } from './output-console';
-import { Simulation } from '../simulation';
+import { SharedTestingModule } from '../../shared/shared-module';
 
 describe('OutputConsole', () => {
-  let component: OutputConsole;
-  let fixture: ComponentFixture<OutputConsole>;
+    let component: OutputConsole;
+    let fixture: ComponentFixture<OutputConsole>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [OutputConsole, Simulation]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [OutputConsole, SharedTestingModule]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(OutputConsole);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(OutputConsole);
+        component = fixture.componentInstance;
+        fixture.componentRef.setInput('console', []);
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
