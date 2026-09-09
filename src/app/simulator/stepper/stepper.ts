082699e7b4027a79af17ea4d44938b7f473ae3a7
Luka van der Plas <lukavdplas@mailfence.com>
2026-09-09 13:27:07 +0200

    disable stepper controls on error etc

--- src/app/simulator/stepper/stepper.ts
+++ src/app/simulator/stepper/stepper.ts
@@ -1,12 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Simulation } from '../simulation-manager';
+import { map } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-stepper',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './stepper.html',
     styleUrl: './stepper.scss',
 })
 export class Stepper {
     simulation = input.required<Simulation>();
+    disable$ = computed(() => this.disableStepper$(this.simulation()));
+
+    private disableStepper$(simulation: Simulation) {
+        return simulation.status$.pipe(
+            map(status => ['error', 'interrupt', 'complete'].includes(status.status)),
+        )
+
+    }
 }
