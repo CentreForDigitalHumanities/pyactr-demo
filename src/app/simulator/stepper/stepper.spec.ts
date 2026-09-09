5d01496d9e08c846602c950994295a2bdcc4786b
Luka van der Plas <lukavdplas@mailfence.com>
2026-09-09 13:36:52 +0200

    add test input

--- src/app/simulator/stepper/stepper.spec.ts
+++ src/app/simulator/stepper/stepper.spec.ts
@@ -1,18 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Stepper } from './stepper';
+import { Simulation } from '../simulation-manager';
+import { TEST_PROVIDERS } from '../../shared/test-providers';
+import { Python } from '../../shared/python';
 
 describe('Stepper', () => {
     let component: Stepper;
+    let simulation: Simulation
     let fixture: ComponentFixture<Stepper>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [Stepper]
+            imports: [Stepper],
+            providers: TEST_PROVIDERS,
         })
             .compileComponents();
 
         fixture = TestBed.createComponent(Stepper);
+        const python = TestBed.inject(Python);
+        simulation = new Simulation(python, 'print(0)')
+        fixture.componentRef.setInput('simulation', simulation);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
